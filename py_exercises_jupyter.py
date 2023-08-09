3343152c47cd9d23dccfd41e420018d720198504
Juanmitaa <112973440+Juanmitaa@users.noreply.github.com>
2023-08-08 23:19:40 -0400

    first commit

---
+++ py_exercises_jupyter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+a = "Hello"
+b = " World"
+
+
+# In[5]:
+
+
+print(a + b)
+
+
+# In[6]:
+
+
+a = 5
+b = 7
+
+addition = print(a + b)
+
+
+# In[7]:
+
+
+c = 8
+d = 21
+multiplication = print(c * d) 
+
+
+# In[ ]:
+
+
+
+
